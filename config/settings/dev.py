072f085cf0c62fd61da68fdc40d4b9600c389fd6
Liza <kravch211218@gmail.com>
2026-09-21 16:07:04 +0200

    Refactor code structure for improved readability and maintainability

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-68ijrqz9^7#es$xs$cdp1g2z5glo#5tjy=$4+b8xc7%)ko@#n("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
